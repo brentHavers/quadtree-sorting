5b44209cad6062dac0f1925fae5ee3fbd059bb05
Brent Havers <brenthavers@gmail.com>
2021-10-22 22:44:41 -0400

    feat: basic quad tree insertion

--- quadtree.py
+++ quadtree.py
@@ -1,15 +1,19 @@
+import rectangle
+
 class QuadTree:
-    def __init__(self, width: int, height: int):
+    def __init__(self, width: int, height: int, node_capacity: int = 4):
         self.width = width
         self.height = height
+        self.node_capacity = node_capacity
 
+        self.items: rectangle = []
+
+    def insert_rectangle(self, rectangle):
+        if len(self.items) < self.node_capacity:
+            self.items.append(rectangle)
+
+    def subdivide(self):
         self.top_left = QuadTree()
         self.top_right = QuadTree()
         self.bottom_left = QuadTree()
         self.bottom_right = QuadTree()
-
-    def insert_rectangle(rectangle):
-        return
-
-    def subdivide():
-        return
